77215d445260d7c61a006b3736a22a07702b0d46
Rodrigo Fuentes <joserodrigofuentes@gmail.com>
2011-02-12 14:45:12 -0500

    added a re-render for invalid email/pw

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,19 @@ class SessionsController < ApplicationController
   end
   
   def create
+    
+    # now need to "authenticate" the submitted information
+    user = User.authenticate(params[:session][:email], 
+                             params[:session][:password])
+    if user.nil? 
+      flash.now[:error] = "Invalid email/password combination."
+      # flash.now is special; it lasts for one "request"
+      @title = "Sign In"
+      render 'new'  # allows me to re-render the "new" page after a failed submit
+    else
+      # Handle successful signin.
+    end
+    
   end
   
   def destroy
